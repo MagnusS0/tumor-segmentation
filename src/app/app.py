2075408d3afbc69ff102510b8aedfc86231dc1b4
Magnus <97634880+MagnusS0@users.noreply.github.com>
2023-11-25 00:28:22 +0100

    feat: Add Streamlit UI for tumor segmentation
    - Introduce a Streamlit app for uploading images and visualizing tumor segmentation.
    - Update AttentionUnet model output handling for Streamlit compatibility.

---
+++ src/app/app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import os
+import tempfile
+from PIL import Image
+from io import BytesIO
+import sys
+sys.path.insert(0, '/home/magsam/workspace/tumor-segmentation/src')
+from model.attention_unet import AttentionUnetModel
+
+
+st.set_page_config(layout="wide", page_title="Tumor Segmentation using Attention U-Net")
+
+st.title("Tumor Segmentation using Attention U-Net")
+st.markdown("""
+    Welcome to the tumor segmentation app! In this application, you can upload 
+    whole-body MIP-PET images of patients with cancer, and the Attention U-Net model 
+    will attempt to segment tumorous areas. For each pixel in the image, the model 
+    predicts whether it belongs to a tumor or a healthy area.
+    
+    **Instructions:**
+    - Upload a Whole-body MIP-PET image of a patient with cancer.
+    - Review the model's segmentation output on the right.
+            
+    **Please note:** This tool is for demonstration and educational purposes only and should not be used for medical 
+    diagnosis or treatment.
+""")
+# Set up the sidebar with instructions and file uploader
+st.sidebar.header("Upload and Process Image :gear:")
+st.sidebar.write("Please upload a MIP-PET image in PNG or JPG format. The maximum file size is 5MB.")
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+# Assuming the AttentionUnetModel class is defined as per your previous code
+model = AttentionUnetModel(model_path="./model/best_metric_model_segmentation2d_dict.pth")
+
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
+def process_image(upload):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as tmpfile:
+        tmpfile.write(upload.getvalue())
+        tmpfile_path = tmpfile.name
+    try:
+        image = Image.open(upload)
+        col1.image(image)
+
+
+
+        # Process the image with the model
+        processed = model.infer(tmpfile_path)
+        col2.image(processed)
+        st.sidebar.markdown("\n")
+        st.sidebar.download_button("Download segmented image", convert_image(processed), "segmented.png", "image/png")
+
+    finally:
+        # Clean up the temporary file
+        os.remove(tmpfile_path)
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+# Check if an image has been uploaded
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        # Process and display the images in the respective columns
+        col1.subheader("Original Image :camera:")
+        col2.subheader("Segmented Image :mag:")
+        process_image(upload=my_upload)
+else:
+    # Display a warning message if no image has been uploaded yet
+    st.sidebar.warning("Awaiting image upload...")
+
+# Placeholder for additional app information or instructions
+st.info("Upload an image to begin analysis. The resulting segmentation will appear on the right.")
--- src/model/attention_unet.py
+++ src/model/attention_unet.py
@@ -3,11 +3,13 @@ from monai.networks.nets import AttentionUnet
 from monai.transforms import Compose, ScaleIntensity, ToTensor, LoadImage, Activations, AsDiscrete, SaveImage
 from monai.inferers import SlidingWindowInferer
 from monai.data.utils import decollate_batch
+from PIL import Image
+import numpy as np
 
 
 class AttentionUnetModel:
     """
-     A class representing a model for tumor segmentation using Attention U-Net.
+    A class representing a model for tumor segmentation using Attention U-Net.
 
     Attributes:
         device (str): The device to run the model on, defaults to 'cuda' or 'cpu'.
@@ -22,11 +24,26 @@ class AttentionUnetModel:
     """
 
     def __init__(self, model_path, device='cuda' if torch.cuda.is_available() else 'cpu'):
+        """
+        Initializes an instance of AttentionUnetModel.
+
+        Args:
+            model_path (str): The path to the pre-trained model.
+            device (str, optional): The device to run the model on. Defaults to 'cuda' if available, else 'cpu'.
+        """
         self.device = device
         self.model = self._load_model(model_path)
 
-    # Set the model with same parameters as in training
     def _load_model(self, model_path):
+        """
+        Loads the Attention U-Net model from the given path.
+
+        Args:
+            model_path (str): The path to the pre-trained model.
+
+        Returns:
+            torch.nn.Module: The loaded Attention U-Net model.
+        """
         model = AttentionUnet(
             spatial_dims=2,
             in_channels=4,
@@ -40,25 +57,58 @@ class AttentionUnetModel:
         model.eval()
         return model
 
-    # Turn input image into tensor
     def _preprocess(self, img):
+        """
+        Preprocesses the input image.
+
+        Args:
+            img (str): Path to the input image.
+
+        Returns:
+            torch.Tensor: The preprocessed image tensor.
+        """
         pre_transforms = Compose([
             ToTensor(),
             LoadImage(image_only=True, ensure_channel_first=True),
             ScaleIntensity()
         ])
-        
-        return pre_transforms(img).unsqueeze(0).to(self.device) # Add batch dimension and send to device
+
+        # Add batch dimension and send to device
+        return pre_transforms(img).unsqueeze(0).to(self.device)
 
     def _postprocess(self, output):
+        """
+        Postprocesses the output from the model.
+
+        Args:
+            output (torch.Tensor): The output tensor from the model.
+
+        Returns:
+            torch.Tensor: The postprocessed output tensor.
+        """
         post_transform = Compose([
             Activations(sigmoid=True),
             AsDiscrete(threshold=0.4),
         ])
 
-        return decollate_batch(post_transform(output[0].cpu())) # Remove batch dimension and send to cpu
-    
-    def infer(self, img, roi_size=(96, 96), sw_batch_size=4, output_dir="./output"):
+        # Remove batch dimension and send to cpu
+        return decollate_batch(post_transform(output[0].cpu()))
+
+    def _tensor_to_image(self, tensor):
+        """
+        Converts a tensor to a PIL Image.
+
+        Args:
+            tensor (torch.Tensor): The tensor to convert.
+
+        Returns:
+            PIL.Image: The converted PIL Image.
+        """
+        array = tensor[0].numpy()
+        array = np.transpose(array, (1, 0))  # Swap the axes
+        return Image.fromarray((array * 255).astype(np.uint8))
+
+    def infer(self, img, roi_size=(96, 96), sw_batch_size=4):
         """
         Run inference on an input image.
 
@@ -66,10 +116,9 @@ class AttentionUnetModel:
             img (str): Path to the input image.
             roi_size (tuple, optional): Size of the ROI to use for inference. Defaults to (96, 96).
             sw_batch_size (int, optional): Batch size for sliding window inference. Defaults to 4.
-            output_dir (str, optional): Directory to save the output images. Defaults to "./output".
 
         Returns:
-            List[str]: Paths of the saved segmentation images.
+            PIL.Image: The segmented image.
         """
         img = self._preprocess(img)
         inferer = SlidingWindowInferer(roi_size, sw_batch_size)
@@ -79,15 +128,11 @@ class AttentionUnetModel:
 
         segmentation = self._postprocess(output)
 
-        saver = SaveImage(
-            output_dir=output_dir,
-            output_ext=".png",
-            output_postfix="seg",
-            scale=255
-        )
-        return saver(segmentation)
+        return self._tensor_to_image(segmentation)
 
 
 if __name__ == "__main__":
-    model = AttentionUnetModel(model_path="./src/model/best_metric_model_segmentation2d_dict (4).pth")
-    model.infer(img="./data/patients/imgs/patient_150.png")
+    model = AttentionUnetModel(
+        model_path="./src/model/best_metric_model_segmentation2d_dict.pth")
+    seg = model.infer(img="./data/patients/imgs/patient_150.png")
+    print(seg)
